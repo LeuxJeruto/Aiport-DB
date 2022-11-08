e2f9978375d0f0245764e7bfa50e6412bac89df6
LeuxJeruto <60471945+LeuxJeruto@users.noreply.github.com>
2022-11-08 19:51:14 +0300

    Added users and passagers

--- flights/views.py
+++ flights/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Flight
+from .models import Flight, Passanger
 
 # Create your views here.
 def index(request):
@@ -11,6 +13,16 @@ def index(request):
 def flight(request, flight_id):
      flight = Flight.objects.get(id=flight_id)
      return render (request, "flights/flight.html", {
-        "flight": flight
+        "flight": flight,
+        "passangers": flight.passangers.all(),
+        "non_passangers": Passanger.objects.exclude(flights=flight).all()
      })
+
+def book(request, flight_id):
+    if request.method== "POST":
+        flight = Flight.objects.get(pk=flight_id)
+        passanger = Passanger.objects.get(pk=int(request.POST["passanger"]))
+        passanger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=(flight.id, )))
+
 
